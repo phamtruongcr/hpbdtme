a63a8c8ad0ac61647fabed7d6b36fc8d19b779e1
TruongPV <truongcr7dz2k@gmail.com>
2026-03-03 23:52:22 +0700

    init

---
+++ tiny-heart-master/tiny-heart-master/birthday-mobile/config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Bé rất đáng iuuu đó, bé có biết không!',
+    introDesc: ` Ý à! bé đọc được dòng này,bé phải mua thuốc đau lưng vì anh làm món quà nhỏ này cho bé đó,anh dởn đấy, happy nha bé:3 `,
+    btnIntro: 'bé bấm vào đi!',
+    title: 'Anh chân thành muốn trao bé một xíu happy ngọt ngào này 🥳',
+    desc: 'Mình mãi là một đôi phải không bé:3 ',
+    btnYes: 'Vâng <33',
+    btnNo: 'Bé không biết :3',
+    question:' Bé muốn gửi gì cho anh hong!',
+    btnReply: 'Gửi luôn anh <3',
+    reply: 'Bé yêu anh Trường nhất <33333333',
+    mess: 'Anh biết bé nói vậy mà, anh chân thành cảm ơn bé:3',
+    messDesc: 'Bấm vào bên dưới đi bé, đừng hồi hộp, anh cũng hồi hộp nữa hihi',
+    btnAccept: 'Bấm nè tin tin <3 ',
+    messLink: 'sinhnhatem-ne-master/happy-birthday-master/index.html' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
